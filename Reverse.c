e6bbd11e87e5324540aab49e7fa835df976078f2
Anwesh Budhathoki <abperni@gmail.com>
2019-10-13 09:02:41 +0545

    Added Sorting Programs

---
+++ Reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,r=0;
+	printf("Enter any number");
+	scanf("%d",&a);
+	do
+	{
+		b=a%10;
+		a/=10;
+		r=10*r+b;
+	}
+	while(a>0);
+	printf("The number in reverse is %d",r);
+	getch();
+}
